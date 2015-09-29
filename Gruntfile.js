4f6b61a5a94b8245fc74b9a0e9ce0b94c8cb8a54
Vitaly Dryukov <miniwe@mail.ru>
2015-09-29 17:29:15 +0200

    added handlebars with layout

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,16 @@
 module.exports = function(grunt) {
 
-    // 1. Вся настройка находится здесь
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        jshint: {
-            myFiles: ['assets/js/**/*.js', '!assets/js/vendor/**/*.js'],
-            options: {
-              curly: true,
-              eqeqeq: true,
-              eqnull: true,
-              browser: true,
-              globals: {
+// 1. Вся настройка находится здесь
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+        myFiles: ['assets/js/**/*.js', '!assets/js/vendor/**/*.js'],
+        options: {
+            curly: true,
+            eqeqeq: true,
+            eqnull: true,
+            browser: true,
+            globals: {
                 jQuery: true
             },
         },
@@ -18,100 +18,110 @@ module.exports = function(grunt) {
     concat: {
         dist: {
             src: [
-                    'assets/js/vendor/*.js', // Все JS в папке libs
-                    'assets/js/app.js'
-                    ],
-                    dest: 'public/js/script.js'
-                }
+                'assets/js/vendor/*.js', // Все JS в папке libs
+                'assets/js/app.js'
+            ],
+            dest: 'public/js/script.js'
+        }
+    },
+    less: {
+        development: {
+            options: {
+                paths: ["assets/less"]
             },
-            less: {
-              development: {
-                  options: {
-                    paths: ["assets/less"]
-                },
-                files: {
-                    "public/css/style.css": "assets/less/style.less"
-                }
+            files: {
+                "public/css/style.css": "assets/less/style.less"
+            }
 
+        },
+        modifyVars: {
+            imgPath: '"/images"',
+            bgColor: 'red'
+        }
+    },
+    uglify: {
+        build: {
+            src: 'public/js/script.js',
+            dest: 'public/js/script.min.js'
+        }
+    },
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'assets/img/',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'public/img/'
+            }]
+        }
+    },
+    handlebarslayouts: {
+        home: {
+            files: {
+                'public/*.html': 'views/*.html'
             },
-            modifyVars: {
-                imgPath: '"/images"',
-                bgColor: 'red'
-            }
-        },
-        uglify: {
-            build: {
-                src: 'public/js/script.js',
-                dest: 'public/js/script.min.js'
-            }
-        },
-        staticHandlebars: {
-            default: {
-
-                options: {
-                    // filesRoot: 'views/assets', //used for JS/CSS files
-                    // packagedFilesPath: 'public',
-                    json: '',
-                    partials:'',
-                    helpers:''
-                },
-                files: {'public/**/*.html': 'views/**/*.hbt'}
-            }
-        },
-
-        // imagemin: {
-        //     dynamic: {
-        //         files: [{
-        //             expand: true,
-        //             cwd: 'assets/img/',
-        //             src: ['**/*.{png,jpg,gif}'],
-        //             dest: 'img/'
-        //         }]
-        //     }
-        // },
-
-        watch: {
-            // templates: {
-            //     files: ['views/**/*.hbt'],
-            //     tasks: ['staticHandlebars'],
-            //     options: {
-            //         spawn: false,
-            //     },
-            // },
-            // scripts: {
-            //     files: ['assets/coffee/*.coffee'],
-            //     tasks: ['coffee', 'jshint', 'concat', 'uglify'],
-            //     options: {
-            //         spawn: false,
-            //     },
-            // },
-            css: {
-                files: ['assets/less/**/*.scss'],
-                tasks: ['less'],
-                options: {
-                    spawn: false,
-                },
+            options: {
+                partials: [
+                'views/partials/*.hbs',
+                'views/layout/layout.html'
+                ],
+                modules: [
+                'views/helpers/helpers-*.js'
+                ],
+                basePath: 'views/',
+                context: {
+                    title: 'TTT',
+                    items: [
+                    'apple',
+                    'orange',
+                    'banana'
+                    ]
+                }
             }
         }
-    });
+    },
+    watch: {
+        templates: {
+            files: ['views/**/*.hbt', 'views/**/*.html'],
+            tasks: ['handlebars'],
+            options: {
+                spawn: false,
+            },
+        },
+        scripts: {
+            files: ['assets/js/**/*.coffee'],
+            tasks: ['jshint', 'concat', 'uglify'],
+            options: {
+                spawn: false,
+            },
+        },
+        css: {
+            files: ['assets/less/**/*.scss'],
+            tasks: ['less'],
+            options: {
+                spawn: false,
+            },
+        }
+    }
+});
 
-    // 3. Тут мы указываем Grunt, что хотим использовать этот плагин
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-static-handlebars');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+// 3. Тут мы указываем Grunt, что хотим использовать этот плагин
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks("grunt-handlebars-layouts");
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+// grunt.loadNpmTasks('grunt-notify');
 
-    // grunt.loadNpmTasks('grunt-contrib-imagemin');
-    // grunt.loadNpmTasks('grunt-notify');
+// grunt.loadNpmTasks('grunt-contrib-coffee');
 
-    // grunt.loadNpmTasks('grunt-contrib-coffee');
+// 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
+// grunt.registerTask('default', ['coffee', 'jshint', 'concat', 'uglify', 'imagemin', 'compass']);
+// grunt.registerTask('short', ['coffee', 'concat', 'uglify', 'compass']);
 
-    // 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
-    // grunt.registerTask('default', ['coffee', 'jshint', 'concat', 'uglify', 'imagemin', 'compass']);
-    // grunt.registerTask('short', ['coffee', 'concat', 'uglify', 'compass']);
-
-    grunt.registerTask('default', ['staticHandlebars', 'concat','jshint','less','uglify']);
+grunt.registerTask('handlebars', ['handlebarslayouts']);
+grunt.registerTask('default', ['handlebars', 'imagemin', 'concat','jshint','less','uglify']);
 
 };
---
+++ views/helpers/helpers-test.js
@@ -0,0 +1,3 @@
+module.exports = function(Handlebars) {
+    return this['TTT'];
+};
